a0532b2662c90febca978d1d1be321e0a6a4df3e
Litriboxer <litriboxer@hotmail.com>
2022-06-26 13:27:13 +0200

    intento añadir colores a sass, no funciona

--- src/componentes/Cabecera.tsx
+++ src/componentes/Cabecera.tsx
@@ -18,7 +18,7 @@ class Cabecera extends React.Component {
         <div className="row">
           <div className="col-8" >
               <h1 className="text-secondary  display-4 text-left pt-5 pb-3 ps-4">Pedro Cuenca Panadero </h1>
-              <h2 className="fst-italic fw-bold display-6 text-left ps-4">Desarrollador Full Stack</h2>
+              <h2 className="fst-italic display-6 text-left ps-4">Desarrollador Full Stack</h2>
           </div>
           <div className="col-4">
               <img src={fotoPedro} className="rounded-circle border border-4 border-secondary img-fluid float-center mt-5 me-3" 
@@ -29,12 +29,12 @@ class Cabecera extends React.Component {
         <hr className="row col-10 mt-5 m-auto placeholder-glow"/>
 
 
-        <ul className="nav flex-column">
+        <ul className="nav flex-column col-3 mt-5 ms-4">
           <li className="nav-item">
-            <a className="nav-link" href="#">Link0</a>
+            <a className="nav-link display-4" href="#">Link0</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link1</a>
+            <a className="nav-link col-3" href="#"><p className="display-6 ">Link1</p></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Link2</a>
