53b17b8aff97df6371fff3421947131740d61328
Litriboxer <litriboxer@hotmail.com>
2022-06-26 03:07:40 +0200

    uso de containers, row y column  para colocar cabecera

--- src/componentes/Cabecera.tsx
+++ src/componentes/Cabecera.tsx
@@ -6,67 +6,50 @@ import '../../node_modules/bootstrap/dist/js/bootstrap';
 
 
 
-class Cabecera extends React.Component{
+class Cabecera extends React.Component {
 
- 
+
 
 
   render(): React.ReactNode {
     return (
-    <div className="bg-gradient">
-      <div className="nombreFoto  d-inline-flex justify-content-between p-3  ">
-        <div className="nombreCargo me-5">
-        <h1 className="text-secondary display-3 text-left pt-5 pe-1 pb-3 ps-5 me-5">Pedro Cuenca Panadero </h1>
-        <h2 className="text-dark font-italic display-6 text-left ps-5">Desarrollador Full Satack</h2>
-        </div>
-        <div className="ms-5 me-5">
-          <div>
-        <img src={fotoPedro} className="miFoto rounded-circle border border-4 border-secondary  img-fluid ms-5" alt="foto cara Pedro Cuenca Panadero"
-         width={300} height={300}  />
-         </div>
+      <div className="bg-gradient row container-xxl-1">
 
-         <div className="modal" >
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+        <div className="row">
+          <div className="col-8" >
+              <h1 className="text-secondary  display-4 text-left pt-5 pb-3 ps-4">Pedro Cuenca Panadero </h1>
+              <h2 className="fst-italic fw-bold display-6 text-left ps-4">Desarrollador Full Stack</h2>
+          </div>
+          <div className="col-4">
+              <img src={fotoPedro} className="rounded-circle border border-4 border-secondary img-fluid float-center mt-5 me-3" 
+                 alt="foto cara Pedro Cuenca Panadero" width={200} height={200} />
+          </div>
         </div>
-        </div>
+
+        <hr className="row col-10 mt-5 m-auto placeholder-glow"/>
 
 
         <ul className="nav flex-column">
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link0</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link1</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link2</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link3</a>
-  </li>
-</ul>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link0</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link1</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link2</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link3</a>
+          </li>
+        </ul>
+      </div>
 
 
 
 
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default Cabecera;
